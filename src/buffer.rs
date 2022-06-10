6989f5af93e0622d27906439fdd0c553deee51ca
Ioan Thomas <29049952+IoanThomas@users.noreply.github.com>
2022-06-10 14:07:35 +0100

    Add write_u16 tests

--- src/buffer.rs
+++ src/buffer.rs
@@ -28,3 +28,26 @@ pub fn write_string(buffer: &mut Vec<u8>, data: impl AsRef<str>) -> result::Resu
 pub fn write_bytes(buffer: &mut Vec<u8>, data: &[u8]) -> result::Result<()> {
     buffer.write_all(data).map_err(|_| Error::BufferWrite)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn write_u16_one_byte() {
+        let mut buffer = vec![];
+        write_u16(&mut buffer, 0x002d).expect("failed to write u16");
+
+        assert_eq!(buffer[0], 0x00);
+        assert_eq!(buffer[1], 0x2d);
+    }
+
+    #[test]
+    fn write_u16_two_bytes() {
+        let mut buffer = vec![];
+        write_u16(&mut buffer, 0x010e).expect("failed to write u16");
+
+        assert_eq!(buffer[0], 0x01);
+        assert_eq!(buffer[1], 0x0e);
+    }
+}
